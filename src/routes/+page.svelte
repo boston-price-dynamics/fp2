5bfd69942e9bbc9c7290bbfe7ec860a1e5aead84
jyoonsong <jaeyoons@mit.edu>
2024-04-07 16:34:41 -0400

    import csv

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,11 +8,9 @@
     "pk.eyJ1Ijoianlvb25zb25nIiwiYSI6ImNsdW95emJzMzIxMDQya3FwcXc1NzA4c2sifQ.C9_2pEU8Z00WnIWNndTg_Q";
 
   let map;
-  let stations = [];
-  let trips = [];
+  let properties = [];
+  let assessments = [];
   let mapViewChanged = 0;
-  let departures;
-  let arrivals;
   let radiusScale = 0;
   let yearFilter = 2017;
 
@@ -22,10 +20,10 @@
     return { cx: x, cy: y };
   }
 
-  $: radiusScale = d3
-    .scaleSqrt()
-    .domain([0, d3.max(stations, (d) => d.totalTraffic)])
-    .range([0, 25]);
+  //   $: radiusScale = d3
+  //     .scaleSqrt()
+  //     .domain([0, d3.max(stations, (d) => d.totalTraffic)])
+  //     .range([0, 25]);
 
   // listener
   $: map?.on("move", (evt) => mapViewChanged++);
@@ -41,33 +39,9 @@
 
     await new Promise((resolve) => map.on("load", resolve));
 
-    // blue bike stations
-    stations = await d3.csv(
-      "https://vis-society.github.io/labs/8/data/bluebikes-stations.csv"
+    properties = await d3.csv(
+      "https://raw.githubusercontent.com/boston-price-dynamics/fp2/main/assess_lat_long.csv"
     );
-
-    // blue bike station traffic
-    trips = await d3.csv(
-      "https://vis-society.github.io/labs/8/data/bluebikes-traffic-2024-03.csv"
-    );
-
-    departures = d3.rollup(
-      trips,
-      (v) => v.length,
-      (d) => d.start_station_id
-    );
-    arrivals = d3.rollup(
-      trips,
-      (v) => v.length,
-      (d) => d.end_station_id
-    );
-    stations = stations.map((station) => {
-      let id = station.Number;
-      station.departures = departures.get(id) ?? 0;
-      station.arrivals = arrivals.get(id) ?? 0;
-      station.totalTraffic = departures.get(id) + arrivals.get(id) ?? 0;
-      return station;
-    });
   });
 </script>
 
@@ -86,7 +60,7 @@
 <div id="map">
   <svg>
     {#key mapViewChanged}
-      {#each stations as station}
+      <!-- {#each stations as station}
         <circle
           {...getCoords(station)}
           r={radiusScale(station.totalTraffic)}
@@ -99,7 +73,7 @@
             arrivals)</title
           >
         </circle>
-      {/each}
+      {/each} -->
     {/key}
   </svg>
 </div>
